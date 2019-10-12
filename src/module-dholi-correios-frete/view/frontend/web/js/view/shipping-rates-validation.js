4d35bf1d78363e64755f79daecc7585aa596b8b7
Rodrigo Sanders <contato@eloom.com.br>
2019-10-12 19:59:12 -0300

    Validadores

---
+++ src/module-dholi-correios-frete/view/frontend/web/js/view/shipping-rates-validation.js
@@ -0,0 +1,1 @@
+define(["uiComponent","Magento_Checkout/js/model/shipping-rates-validator","Magento_Checkout/js/model/shipping-rates-validation-rules","Dholi_CorreiosFrete/js/model/shipping-rates-validator","Dholi_CorreiosFrete/js/model/shipping-rates-validation-rules"],function(b,c,a,d,e){c.registerValidator("dholi_correios",d);a.registerRules("dholi_correios",e);return b});
